f417170a9ea2c5d47c3b660adda58515e76685be
Josue Henrique <josue21henrique@gmail.com>
2025-04-17 11:18:52 -0300

    correção no nome

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "http://212.85.19.3:3003",
-    baseURL: "http://localhost:3003",
+    baseURL: "http://212.85.19.3:3003",
+    // baseURL: "http://localhost:3003",
 });
 
 // api.interceptors.request.use(config => {
