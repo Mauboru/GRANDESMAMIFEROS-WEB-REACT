0f49da529a1e829d0454d44f41b05afdda8cff45
mauboru <josue21henrique@gmail.com>
2025-04-18 13:53:59 -0300

    corrigindo rota teste para ficar automatico

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://212.85.19.3:3003",
-    // baseURL: "http://localhost:3003",
-});
+const primaryURL = "http://localhost:3003";
+const fallbackURL = "http://212.85.19.3:3003";
+
+const api = axios.create();
+
+export const requestWithFallback = async (config) => {
+  try {
+    return await api({ ...config, baseURL: primaryURL });
+  } catch (err) {
+    if (err.code === "ECONNREFUSED" || err.code === "ERR_NETWORK") {
+      return await api({ ...config, baseURL: fallbackURL });
+    }
+    throw err;
+  }
+};
+
 
 // api.interceptors.request.use(config => {
 //     const token = localStorage.getItem("token");
